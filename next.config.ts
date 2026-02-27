7313fd10374d897f0d2b06216599f3e61aa493c4
Promethees <tqmthong@gmail.com>
2026-02-27 14:26:16 +0700

    Modify to run offline

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    turbopack: {
+        root: __dirname,
+    },
+    experimental: {
+        mcpServer: false,
+    }
 };
 
 export default nextConfig;
